a371cf11bb9105913848e3303563cd4a2342e1a7
hyeon0415 <mandol7878@hs.ac.kr>
2021-05-30 23:08:48 +0900

    0530p

---
+++ niceApp/Pods/Target Support Files/Pods-niceApp/Pods-niceApp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_niceApp : NSObject
+@end
+@implementation PodsDummy_Pods_niceApp
+@end
---
+++ niceApp/niceApp/ViewControllers/MainViewController.swift
@@ -0,0 +1,63 @@
+//
+//  ViewController.swift
+//  niceApp
+//
+//  Created by 양현 on 2021/05/30.
+//
+
+import UIKit
+import Lottie
+
+class MainViewController: UIViewController {
+    
+    let titleLabel: UILabel = {
+       let label = UILabel()
+        label.textColor = .black
+        label.textAlignment = .center
+        label.text = "메인화면"
+        label.font = UIFont.boldSystemFont(ofSize: 70)
+        return label
+    }()
+    
+    let animationView: AnimationView = {
+        let animView = AnimationView(name: "12358-christmas-loading")
+        animView.frame = CGRect(x:0,y:0, width:400, height:400)
+        
+        animView.contentMode = .scaleAspectFill
+        
+        
+        return animView
+    }()
+
+    //뷰가 생성되었을때
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .black
+        view.addSubview(animationView)
+        animationView.center = view.center
+        
+        
+        
+        //애니메이션 실행
+        animationView.play{ (finish) in
+            //
+            print("애니메이션이 끝났다.")
+            
+            self.view.backgroundColor = .white
+            self.animationView.removeFromSuperview()
+            
+            self.view.addSubview(self.titleLabel)
+            
+            self.titleLabel.translatesAutoresizingMaskIntoConstraints = false
+            self.titleLabel.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+            self.titleLabel.centerYAnchor.constraint(equalTo: self.view.centerYAnchor).isActive = true
+            
+        }
+    }
+
+
+}
+
+
