a365d7bd2b41b98ba47aee29a016b17ea0058542
hyeon0415 <mandol7878@hs.ac.kr>
2021-06-01 20:34:38 +0900

    0601

---
+++ Extension_practice/Extension_practice/Extension.swift
@@ -0,0 +1,16 @@
+//
+//  Extension.swift
+//  Extension_practice
+//
+//  Created by 양현 on 2021/06/01.
+//
+
+import Foundation
+import UIKit
+
+extension UIViewController{
+    
+    func sayHi()  {
+        print("하이!!")
+    }
+}
---
+++ Extension_practice/Extension_practice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Extension_practice
+//
+//  Created by 양현 on 2021/06/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        sayHi()
+    }
+
+
+}
+
