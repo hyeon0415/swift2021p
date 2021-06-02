553edfab65053ee992fa8adba148e4f591f6da69
hyeon0415 <mandol7878@hs.ac.kr>
2021-06-02 22:58:12 +0900

    0602p

--- MyAlbum/MyAlbum/ViewController.swift
+++ MyAlbum/MyAlbum/ViewController.swift
@@ -8,21 +8,35 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var currentValue = 0
 
+    @IBOutlet weak var priceLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+       refresh()
         
     }
 
     @IBAction func showAlert(_ sender: Any) {
+    
+    let message = "가격은 $\(currentValue)입니다."
         
-    let alert = UIAlertController(title: "Hello", message: "My first App!", preferredStyle: .alert)
-    let action = UIAlertAction(title: "OK", style: .default, handler: nil)
+    let alert = UIAlertController(title: "Hello", message: message, preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .default, handler: {action in self.refresh()})
         
         present(alert, animated: true, completion: nil)
         alert.addAction(action)
         
+  
         
+        
+    }
+    func refresh() {
+        let randomPrice = arc4random_uniform(10000) + 1
+            currentValue = Int(randomPrice)
+            priceLabel.text = "$ \(currentValue)"
     }
     
 }
