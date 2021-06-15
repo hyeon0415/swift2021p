d2cad870ec1582fce2610607611b979ce242bc31
hyeon0415 <mandol7878@hs.ac.kr>
2021-06-15 12:17:54 +0900

    0615p

--- BountyList/BountyList/DetailViewController.swift
+++ BountyList/BountyList/DetailViewController.swift
@@ -28,8 +28,10 @@ class DetailViewController: UIViewController {
     @IBOutlet weak var imgView: UIImageView!
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var bountyLabel: UILabel!
+    @IBOutlet weak var nameLabelCenterX: NSLayoutConstraint!
     
-//    var name: String?
+    @IBOutlet weak var bountyLabelCenterX: NSLayoutConstraint!
+    //    var name: String?
 //    var bounty: Int?
     
 //    var bountyInfo: BountyInfo?
@@ -39,8 +41,44 @@ class DetailViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
        updateUI()
-       
+        perpareAnimation()
+}
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        showAnimation()
+        
     }
+    
+    private func perpareAnimation(){
+//        nameLabelCenterX.constant = view.bounds.width
+//        bountyLabelCenterX.constant = view.bounds.width
+        
+        nameLabel.transform = CGAffineTransform(translationX: view.bounds.width, y: 0).scaledBy(x: 3, y: 3).rotated(by: 180)
+        
+        bountyLabel.transform = CGAffineTransform(translationX: view.bounds.width, y: 0).scaledBy(x: 3, y: 3).rotated(by: 180)
+        
+        nameLabel.alpha = 0
+        bountyLabel.alpha = 0
+    }
+    private func showAnimation(){
+        nameLabelCenterX.constant = 0
+        bountyLabelCenterX.constant = 0
+        
+//        UIView.animate(withDuration: 0.3, animations: self.view.layoutIfNeeded)
+        
+//        UIView.animate(withDuration: 0.3, delay: 0.1, options: .curveEaseIn, animations: self.view.layoutIfNeeded, completion: nil)
+        
+//        UIView.animate(withDuration: 0.5, delay: 0.3, usingSpringWithDamping: 0.6, initialSpringVelocity: 2, options: .allowUserInteraction, animations: self.view.layoutIfNeeded, completion: nil)
+//        UIView.transition(with: imgView, duration: 0.3, options: .transitionFlipFromLeft, animations: nil, completion: nil)
+        
+        UIView.animate(withDuration: 1, delay: 0, usingSpringWithDamping: 0.6, initialSpringVelocity: 2, options: .allowUserInteraction, animations: { self.nameLabel.transform = CGAffineTransform.identity
+            self.bountyLabel.transform = CGAffineTransform.identity}, completion: nil)
+        self.nameLabel.alpha = 1
+        self.bountyLabel.alpha = 1
+        
+        UIView.transition(with: imgView, duration: 0.3, options: .transitionFlipFromLeft, animations: nil, completion: nil)
+       
+}
     
     func updateUI() {
         if let bountyInfo = self.viewmodel.bountyInfo{
@@ -54,7 +92,6 @@ class DetailViewController: UIViewController {
     @IBAction func close(_ sender: Any) { dismiss(animated: true, completion: nil)
     }
     
-
 }
 class DetailViewModel {
     var bountyInfo: BountyInfo?
