422c69a9e2b966d5a2f515a075c6aca880d5e956
hyeon0415 <mandol7878@hs.ac.kr>
2021-06-16 15:03:05 +0900

    0616p

---
+++ MyPractice1.playground/Contents.swift
@@ -0,0 +1,26 @@
+import UIKit
+
+// 변수
+var str = "Hello, playground"
+
+var age = 10
+
+
+age = 20
+age = 30
+age = 55
+str = "hello"
+str = "hi"
+
+// swift 언어 특징
+// safe type 타입 안전한, 강타입 타입이 엄격
+
+//javascript는 됨
+//age = "hi"
+//str = 111
+
+
+// 상수 값이 변하지 않음
+let guestName = "John"
+
+let userID = "aaabbbcc@demo.com"
