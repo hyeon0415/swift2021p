028ff0e29763d4c9483ac678bfc07c7ab48fb58a
hyeon0415 <mandol7878@hs.ac.kr>
2021-06-20 17:37:20 +0900

    0620p

--- inflearn/MyPractice16.playground/Contents.swift
+++ inflearn/MyPractice16.playground/Contents.swift
@@ -1,3 +1,42 @@
 import UIKit
 
+// Structure
 
+
+
+
+
+// value type 사용할때마다 새로운 구조로 사용됨. 다른 곳과 연결되는 것이 아님
+struct ImageType {
+    var type = ""
+    var width = 0
+    var height = 0   
+}
+var imageType1 = ImageType()
+var imageType2 = imageType1
+var imageType3 = imageType2
+
+imageType1.type = "jpg"
+imageType2.type = "png"
+imageType3.type
+
+
+imageType1.type
+imageType2.type
+
+
+
+class MyInfo {
+    var myAge = 0
+    
+}
+var info1 = MyInfo()
+var info2 = info1
+var info3 = info2
+
+info1.myAge = 20
+info2.myAge = 10
+
+info1.myAge
+info2.myAge
+info3.myAge
